66748b210c816cafe4680ad38f2b44b3deda72d2
lmxy0212 <lmxyngnl@gmail.com>
2018-07-13 17:19:24 +0000

    token done

--- token.cpp
+++ token.cpp
@@ -11,30 +11,32 @@ Token get_token(){
     switch(ch){
         case 'q': case ';': case '(': case '+': case '-': case '*': case '/': case ')':
             return Token{ch};
-        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.':
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.':{
             cin.putback(ch);// read more digit
             double val;
             cin >> val;
             return Token{'8',val};
-            
+        }default:
+            return Token{'e',double(ch)};
     }
 };
 
 vector <Token> tokens;
 int main(){
-    Token t = get_token();
-    while( t.kind != 'q'){
+    for(Token t = get_token();t.kind != 'q';t = get_token()){
         tokens.push_back(t);
-        t = get_token();
     }
     for( Token token : tokens){
         if(token.kind == '8') {
-            cout << "token kind number" << " with value " << token.value << "\n";
+            cout << "A number token with value = " << token.value << "\n";
         }
         else{
-            cout << "token kind " <<token.kind << "\n";
+            if(token.kind == 'e'){
+                cout << "We received an invalid token of value " << char(token.value) << "\n";
+            }else{
+                cout << "A token of kind " <<token.kind << "\n";
+            }
         }
         
     }
 }
-
