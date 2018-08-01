3415cfaf0a2ed249f33670a6d039769cfeaf84f0
lmxy0212 <lmxyngnl@gmail.com>
2018-08-01 14:44:58 +0000

    today's drill/

---
+++ memTest.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+    int i = 0;
+    while(true){
+        double* dptr = new double[10000];
+        cout << "Loop " << i << endl;
+        cout << "dptr[0] = " << dptr[0] << endl;
+        i++;
+    }
+
+}
---
+++ stackTest.py
@@ -0,0 +1,5 @@
+def increment(i):
+	print("i = " + str(i))
+	increment(i+1)
+
+increment(0)
